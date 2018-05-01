be0e64fffffebad5881b154ef949d8871c28f178
Conrad Irwin <conrad.irwin@gmail.com>
2018-04-30 19:20:18 -0700

    Use a less resource-intensive notify library
    This allows you to use lrt on a mac without having to increase the
    number of open file-descriptors. As a result we can also be more precise
    about file-edits, and will track edits to any code included by your
    program.
    
    It is slightly slower (I had to set the timeout to 200ms from 100ms to
    make the tests pass) but that should still be within the realm of
    reasonable for making new web-requests.

--- main.go
+++ main.go
@@ -33,8 +33,8 @@ import (
 	"syscall"
 	"time"
 
-	"github.com/fsnotify/fsnotify"
 	shellwords "github.com/mattn/go-shellwords"
+	"github.com/rjeczalik/notify"
 )
 
 // raw arguments
@@ -68,7 +68,7 @@ var (
 	waiter  sync.WaitGroup
 	tmpFile *os.File
 
-	watcher    *fsnotify.Watcher
+	eventChan  chan notify.EventInfo
 	watchedDir = map[string]bool{}
 )
 
@@ -122,13 +122,9 @@ func (b *blockingProxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 // rebuildOnChange sets up all the watches and the rebuilder
 func rebuildOnChange() {
-	var err error
-	watcher, err = fsnotify.NewWatcher()
-	if err != nil {
-		fmt.Fprint(os.Stderr, "lrt: "+err.Error())
-		os.Exit(1)
-	}
-	defer watcher.Close()
+	eventChan = make(chan notify.EventInfo)
+	defer notify.Stop(eventChan)
+	defer close(eventChan)
 
 	rebuilder := debounceCallable(100*time.Millisecond, rebuild)
 	go rebuilder()
@@ -151,15 +147,10 @@ func rebuildOnChange() {
 	for {
 		select {
 		// watch for events
-		case ev := <-watcher.Events:
-			if (strings.HasSuffix(ev.Name, ".go") && !strings.HasSuffix(ev.Name, "_test.go")) && ev.Op != fsnotify.Chmod {
+		case ev := <-eventChan:
+			if strings.HasSuffix(ev.Path(), ".go") && !strings.HasSuffix(ev.Path(), "_test.go") {
 				go rebuilder()
 			}
-
-			// watch for errors
-		case err := <-watcher.Errors:
-			fmt.Fprintln(os.Stderr, "lrt: "+err.Error())
-			os.Exit(1)
 		}
 	}
 }
@@ -199,7 +190,7 @@ func rebuild() {
 
 	stopRunningService()
 
-	args := append(buildArgs, "-o", tmpFile.Name(), "-i", "-v", packageName)
+	args := append(buildArgs, "-o", tmpFile.Name(), "-v", packageName)
 	output, err := exec.Command("go", append([]string{"build"}, args...)...).CombinedOutput()
 
 	if err != nil {
@@ -310,20 +301,17 @@ func watchListedPackages(output []byte) {
 			os.Exit(1)
 		}
 
-		// exclude vendor to prevent running out of file descriptors.
-		if !pkg.Goroot {
-			if !watchedDir[pkg.Dir] {
-				err = watcher.Add(pkg.Dir)
-				if err != nil {
-					fmt.Fprintf(os.Stderr, "lrt: "+err.Error()+"\n")
-					if strings.Contains(err.Error(), "too many open files") {
-						fmt.Fprintf(os.Stderr, "     hint: you may need to increase the number of open files you are allowed, try:\n")
-						fmt.Fprintf(os.Stderr, "           sudo launchctl limit maxfiles 1000000 1000000\n")
-					}
-					os.Exit(1)
+		if !watchedDir[pkg.Dir] {
+			err = notify.Watch(pkg.Dir, eventChan, notify.Write|notify.Remove|notify.Create)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "lrt: "+err.Error()+"\n")
+				if strings.Contains(err.Error(), "too many open files") {
+					fmt.Fprintf(os.Stderr, "     hint: you may need to increase the number of open files you are allowed, try:\n")
+					fmt.Fprintf(os.Stderr, "           sudo launchctl limit maxfiles 1000000 1000000\n")
 				}
-				watchedDir[pkg.Dir] = true
+				os.Exit(1)
 			}
+			watchedDir[pkg.Dir] = true
 		}
 
 	}
--- main_test.go
+++ main_test.go
@@ -108,7 +108,7 @@ func getStringResponse(t *testing.T, url *url.URL) string {
 }
 
 func waitForFsNotify() {
-	time.Sleep(100 * time.Millisecond)
+	time.Sleep(200 * time.Millisecond)
 }
 
 func TestLrt(t *testing.T) {
@@ -118,7 +118,7 @@ func TestLrt(t *testing.T) {
 	response := getStringResponse(t, listenURL)
 
 	if response != "lrt/test: OK" {
-		t.Errorf("Got unexpected response from lrt: %s", response)
+		t.Errorf("Got unexpected response from lrt: %#v", response)
 	}
 }
 
@@ -129,7 +129,7 @@ func TestLrt_Rebuild(t *testing.T) {
 	response := getStringResponse(t, listenURL)
 
 	if response != "lrt/test: OK" {
-		t.Errorf("Got unexpected response from lrt: %s", response)
+		t.Errorf("Got unexpected response from lrt: %#v", response)
 	}
 
 	defer os.Remove("test/override.go")
@@ -145,7 +145,15 @@ func TestLrt_Rebuild(t *testing.T) {
 
 	response = getStringResponse(t, listenURL)
 	if response != "lrt/test: OVERRIDE" {
-		t.Errorf("Got unexpected response from lrt: %s", response)
+		t.Errorf("Got unexpected response from lrt: %#v", response)
+	}
+
+	os.Remove("test/override.go")
+	waitForFsNotify()
+
+	response = getStringResponse(t, listenURL)
+	if response != "lrt/test: OK" {
+		t.Errorf("Got unexpected response from lrt: %#v", response)
 	}
 }
 
